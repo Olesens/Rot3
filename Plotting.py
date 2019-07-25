5b9487cd779b3fd6e8d2d017e1a80c2463b6d6f6
olesens <saraholesen@hotmail.com>
2019-07-25 13:57:17 +0100

    baseline

--- Plotting.py
+++ Plotting.py
@@ -6,7 +6,7 @@ import ipython_genutils
 
 
 # load in file from Rot3_data folder using pickle
-pickle_in = open("Rot3_data\\Animal_df.pkl","rb")
+pickle_in = open("Rot3_data\\Rat_full_df.pkl","rb")
 Animal_df = pickle.load(pickle_in)
 Animal_df = Animal_df.sort_index()  # sort according to index, this should sort according to date
 
