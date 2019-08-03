9a8a2fa0d4dcebdba847c90f7dcda5c945827f23
Sarah <saraholesen@hotmail.com>
2019-08-03 17:41:24 +0100

    plots included

--- Plotting.py
+++ Plotting.py
@@ -78,50 +78,74 @@ def plot_cp(df, stage='All'):
     return cp_plot
 
 
-def plot_trials(df, stage='All', inc_vio=True, vio_only=False, tm_only=False):
-    """
-    :param df: dataframe to use for plotting
-    :type df: dataframe
-    :param stage: what stages to include
-    :type stage: string or int
-    :param inc_vio:
-    :param vio_only: only plot
-    :param tm_only: only plots timeouts
-    :return: plot of certain trials over time for given df
-    """
+def plot_trials(df, stage='All', p_type = 'all_trials', inc_vio=True, perc = True, ):
+
+    p_types = ['all_trials', 'vio_only', 'tm_only', 'left', 'right']
+    if p_type not in p_types:
+        raise ValueError("Invalid p_type. Expected one of: %s" % p_type)
+
     # Filter according to stages
     if stage != 'All':
-        mask = df['stage']== stage
+        mask = df['stage'] == stage
         df = df[mask]
     else:
         print('all stages included')
-    # Labels for plot title
-    violations_label = ''
-    title = ': Done trials for PWM animals '
-    if vio_only is True:
-        df_trials = (df['done_trials'] * df['violations'])
-        title = ': Violation trials for PWM animals'
-    elif tm_only is True:
-        df_trials = (df['done_trials'] * df['timeouts'])
-        title = ': Timeout trials for PWM animals'
-    elif inc_vio is True:
-        df_trials = df['done_trials'] - (df['done_trials'] * df['violations'])
-        violations_label = 'minus violations'
-    else:
-        df_trials = df['done_trials']
 
+    if p_type is 'left' or 'right':  # might not need to be if statement, just do no matter what
+        # this should only occur inside the function so change be global just for the division.
+        # how would it effect the other calculations
+
+         # replace
+        df['done_trials'] = df['done_trials'] - ((df['violations'] + df['timeouts'])*df['done_trials'])
+        mask = df['done_trials'] == 0
+        df['done_trials'] = df['done_trials'].mask(mask, 1)
+
+        #df_correct = df['done_trials'] - (df['done_trials'] * df['violations']) - (df['done_trials'] * df['timeouts'])
+    p_types_dict = {'all_trials': df['done_trials'] - (df['done_trials'] * df['violations']),  #not %
+                    'vio_only': df['violations'] * 100,
+                    'tm_only': df['timeouts']*100,
+                    'left': df['left_trials'] / df['done_trials']*100,  #might have to make this minus vio and timeouts
+                    'right': df['left_trials'] / df['done_trials']*100}
+
+    p_types_title = {'all_trials': ': Done trials for PWM animals minus violations ',
+                    'vio_only': ': % Violation trials for PWM animals',
+                    'tm_only': ': % Timeout trials for PWM animals',
+                    'left': '% left trials for PWM animals',
+                    'right': '% right trials for PWM animals'}
+
+     #if trials = true:
+     #   take the thing and multiply by done_trials
+    df_trials = p_types_dict[p_type]
     col_list = df_trials.columns.values  # get list to use for labelling
     tri_plot = plt.plot(df_trials,
                         marker='o',
                         linewidth=1.0,
                         markersize=2.5)
-    if vio_only is True:
-        plt.ylim([0, 200])
+    if p_type is not 'all_trials':
+        plt.ylim([-5, 105])
+
     plt.xticks(rotation=75)
     plt.legend(col_list)
     plt.xlabel('Date')
     plt.ylabel('Trials')
-    plt.title('Stage ' + str(stage) + title + str(violations_label))
+    plt.title(p_types_title[p_type])
+
+    # Labels for plot title
+    violations_label = ''
+    #title = ': Done trials for PWM animals '
+
+    # if vio_only is True:
+
+    #   df_trials = (df['done_trials'] * df['violations'])
+    #title = ': Violation trials for PWM animals'
+    #if tm_only is True:
+       # df_trials = (df['done_trials'] * df['timeouts'])
+        #title = ': Timeout trials for PWM animals'
+    #elif inc_vio is True:
+        #df_trials = df['done_trials'] - (df['done_trials'] * df['violations'])
+        #violations_label = 'minus violations'
+    #else:
+        #df_trials = df['done_trials']
 
     # plt.plot instead of df.plot fixed my problem with the x-axis, but the colors are worse, and legends are gone\n",
     # ideally figure out how to fix things in either\n",
@@ -185,7 +209,7 @@ def run_all_plots():
 
 
 
-pwm_trials = Animal_df['done_trials'] - (Animal_df['violations']*Animal_df['done_trials']) - (Animal_df['timeouts']*Animal_df['done_trials']) - Animal_df['left_trials'] - Animal_df['right_trials']
-Animal_df2 = Animal_df.copy()
-Animal_df2['trial_diff'] = pwm_trials
+#pwm_trials = Animal_df['done_trials'] - (Animal_df['violations']*Animal_df['done_trials']) - (Animal_df['timeouts']*Animal_df['done_trials']) - Animal_df['left_trials'] - Animal_df['right_trials']
+#Animal_df2 = Animal_df.copy()
+#Animal_df2['trial_diff'] = pwm_trials
 
